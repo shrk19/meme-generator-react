e6950e1bd31c012f63634b512a4f69b9fea110fe
shrk19 <shreyakri19@gmail.com>
2022-08-05 22:54:27 +0530

    generate meme function working fine

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,13 +3,22 @@ import React, {useState} from 'react'
 
 export default function Meme() {
 
+
+  // to store the array of meme data
   const [allMemeData, setAllMemeData] = useState({});
+
+  // random meme index
+  const [memeIndex, setMemeIndex] = useState(0);
   
-  const [formData, setFormData] = useState({
-    topText: "", 
-    bottomText: "", 
-    memeImage: "https://i.imgflip.com/39t1o.jpg"
-  });
+  // meme template image
+  // const [formData, setFormData] = useState({
+  //   // topText: "", 
+  //   // bottomText: "", 
+  //   memeImage: "https://i.imgflip.com/39t1o.jpg"
+  // });
+
+  // meme caption 
+  const [captions, setCaptions] = useState([]);
   
   React.useEffect(() => {
     fetch("https://api.imgflip.com/get_memes")
@@ -17,32 +26,70 @@ export default function Meme() {
         .then(data => setAllMemeData(data.data.memes))
   }, [])
 
+  React.useEffect(()=>{
+    if(allMemeData.length){
+      setCaptions(Array(allMemeData[memeIndex].box_count).fill(""));
+    }
+  }, [memeIndex, allMemeData]);
+
+  React.useEffect(()=>{
+    console.log(captions)
+  }, [captions])
+
   function getImage(){
-    let random = Math.floor(Math.random()*100);
-    let randomImage = allMemeData[random].url;
-    setFormData((prevMeme)=>{
-      return {
-        ...prevMeme, 
-        memeImage: randomImage
-      }
-    });
-
+    setMemeIndex(Math.floor(Math.random()*100));
+    // let random = Math.floor(Math.random()*100);
+          // let randomImage = allMemeData[memeIndex].url;
+          // setFormData(()=>{
+          //   return {
+          //     // ...prevMeme, 
+          //     memeImage: randomImage
+          //   }
+          // });
   }
-
-  function handleChange(event){
-    const {name, value} = event.target
-    setFormData(prevMeme=>{
-        return {
-            ...prevMeme, 
-            [name] : value
+  function updateCaption(event, index){
+    const text = event.target.value || "";
+    setCaptions(
+      captions.map((c,i)=>{
+        if(index === i){
+          return text;
+        }else{
+          return c;
         }
-    })
+      })
+    )
   }
+  function generateMeme(){
+    const currentMeme = allMemeData[memeIndex];
+    const formData = new FormData();
+    
+    formData.append("username", process.env.REACT_APP_MEME_API_USERNAME);
+    formData.append("password", process.env.REACT_APP_MEME_API_PASSWORD);
+    formData.append("template_id", currentMeme.id);
+    captions.forEach((c, index)=> formData.append(`boxes[${index}][text]`, c));
+
+    fetch("https://api.imgflip.com/caption_image", {
+      method: "POST",
+      body: formData
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+
+  }
+
+  // function handleChange(event){
+  //   const {name, value} = event.target
+  //   setFormData(prevMeme=>{
+  //       return {
+  //           ...prevMeme, 
+  //           [name] : value
+  //       }
+  //   })
+  // }
 
   return (
-    <main>
       <div className='form'>
-        <input 
+        {/* <input 
           type="text" 
           placeholder='top text'
           className='form-input'
@@ -57,7 +104,8 @@ export default function Meme() {
           name='bottomText'
           value={formData.bottomText}
           onChange={handleChange}
-        />
+        /> */}
+
         <button 
           className="form-button" 
           type='submit'
@@ -65,13 +113,29 @@ export default function Meme() {
             Get a new meme image 🖼
         </button>
 
-        <div className="meme">
-          <img className='meme-image' src={formData.memeImage} alt="meme-template" />
+
+        {
+          captions.map((c, index)=>
+            <input key={index} type="text" className="form-input" placeholder={`text ${index+1}`} onChange={(event)=>updateCaption(event, index)}/>
+          )
+        }
+
+        <button 
+          className="form-button" 
+          type='submit'
+          onClick={generateMeme}>
+            Generate Meme
+        </button>
+        {allMemeData.length ? <img className='meme-image' src={allMemeData[memeIndex].url} alt="meme-template" /> : <></>}
+        
+        {/* <div className="meme">
           <h2 className="meme--text top">{formData.topText}</h2>
           <h2 className="meme--text bottom">{formData.bottomText}</h2>
-        </div> 
+          
+        </div>  */}
     </div>
-    </main>
+    
+   
     
   )
 }
